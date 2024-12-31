d554aeb16bd1c2f4b022f3caa97ddfd583202f20
Saurav Singh <snipextt@gmail.com>
2024-12-31 13:33:40 +0530

    src -> lib

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,24 @@ import { peerDependencies } from "./package.json";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	build: {
-		lib: {
-			entry: "./src/index.ts",
-			name: "@wacht/react",
-			fileName: (format) => `index.${format}.js`,
-			formats: ["cjs", "es"],
-		},
-		rollupOptions: {
-			external: [...Object.keys(peerDependencies), "react/jsx-runtime"],
-			output: {
-				globals: {
-					react: "React",
-					"react/jsx-runtime": "jsxRuntime",
-				},
-			},
-		},
-		sourcemap: true,
-		emptyOutDir: true,
-	},
-	plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "./lib/index.ts",
+      name: "@wacht/react",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies), "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
 });
